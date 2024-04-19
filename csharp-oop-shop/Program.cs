556ac106df3d6f5b143312cc1ec1fb849ce08d03
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-04-19 15:17:05 +0200

    Aggiungere i file di progetto.

---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace csharp_oop_shop
+{
+    public class Prodotto 
+    {
+        private int _codice { get; set; }
+        private string _nome { get; set; }
+        private string _descrizione { get; set; }
+        private decimal _prezzo { get; set; }
+        private decimal _iva {  get; set; }
+
+
+
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+// Static: funzione static: metodo che promette di non modificare i valori che vado ad immettere
